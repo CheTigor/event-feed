97996cc4032ff09f9ed301fec1dd0a8278272238
CheTigor <chetvertakov.igor@yandex.ru>
2023-10-07 00:04:18 +0300

    fix: static vars in EventServiceImpl

--- stats-server/common-dto/src/main/java/ru/practicum/constants/Constants.java
+++ stats-server/common-dto/src/main/java/ru/practicum/constants/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String STAT_URL = "http://stats-server:9090";
-    public final static String MAIN_SERVICE_NAME = "emw-main-service";
-    public final static String START_TIME = "2020-01-01 00:00:00";
-    public final static String END_TIME = "2300-01-01 00:00:00";
+    public static final String MAIN_SERVICE_NAME = "emw-main-service";
+    public static final String START_TIME = "2020-01-01 00:00:00";
+    public static final String END_TIME = "2300-01-01 00:00:00";
 }
